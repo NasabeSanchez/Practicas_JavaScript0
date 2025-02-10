62f15d288288562b47f42c4943fffe35e1f30d91
NasabeSanchez <natalicampanota@gmail.com>
2025-02-09 22:58:56 -0600

    Commit 1

---
+++ Tipos_de_dato.js
@@ -0,0 +1,10 @@
+let nombre="Natalia";
+let edad="27";
+let esMayordeEdad= true;
+let saldo;
+let id= Symbol("id");
+let numeroGrande= 123456789101112131415n;
+
+console.log(typeof nombre,edad,esMayordeEdad,saldo,id, numeroGrande);
+console.log(nombre,edad,esMayordeEdad,saldo,id, numeroGrande);
+
