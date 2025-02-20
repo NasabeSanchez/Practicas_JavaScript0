d1b2d2423277c18bde5ce37a1d428ba7ee29f8ca
Natalia Sanchez B <natalicampanota@gmail.com>
2025-02-20 03:45:36 +0000

    "Modificacion a mi primer codigo if"

--- Comparaciones_y_decisiones_en_JavaScript.js
+++ Comparaciones_y_decisiones_en_JavaScript.js
@@ -3,10 +3,10 @@ console.log('La calificacion del estudiante es:'+ calificacion)
 if (calificacion >= 90){
     console.log('Excelente')
 }
-else if(calificacion >=75 && calificacion <=89){
+else if(calificacion <=89 && calificacion >=75){
     console.log('Bien')
 }
-else if( calificacion >= 60 || calificacion <= 74){
+else if( calificacion <= 74 && calificacion >= 60){
     console.log('Suficiente')
 }
 else{
