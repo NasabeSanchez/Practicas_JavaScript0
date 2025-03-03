3555b14229b69875acb97fb17f899ccad2868eb5
Natalia Sanchez B <natalicampanota@gmail.com>
2025-03-03 00:01:01 +0000

    Version 5 pagina de comentarios

--- Proyecto Final/home.js
+++ Proyecto Final/home.js
@@ -1,17 +1,17 @@
-function post(){
+function postear(){
     const commentText=document.getElementById('Lugar_Comentario').value;
-    const commentSeccion=document.getElementById('coment_section');
-    const username= localStorage.getItem('Usuario_Logeado');
-    const Fecha=new Date().toLocaleTimeString()
+    const commentSeccion=document.querySelector('.coment_section');
+    const username= localStorage.getItem('Usuario_Logeado') || "Anónimo";
+    const Fecha=new Date().toLocaleTimeString();
 
     if(commentText.trim()===""){ //trim elimina los espacios vacios al inicio de una cadena
-        alert("No se puedem publicar comentarios vacios, escribe algo")
-        return
+        alert("No se pueden publicar comentarios vacios, escribe algo")
+        return;
     }
 
-    const comentario= document.createElement('seccion')
-    comentario.classList.add("comentario")
-    comentario.innerHTML= '<strong>${username}</strong> - %{fecha} <br> ${commentText}';
+    const comentario= document.createElement('section');
+    comentario.classList.add('comentario');
+    comentario.innerHTML= `<strong>${username}</strong> - ${Fecha} <br> ${commentText}`;
 
     commentSeccion.appendChild(comentario);
     document.getElementById('Lugar_Comentario').value="";
